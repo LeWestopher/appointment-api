4a18b4ec81fcf992ae4e35b06b2ea15e134b11cc
Wes King <wes@boone.io>
2017-01-12 21:31:01 -0600

    Added database access, added response formatting utils, built out most of Users resource.

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,27 @@ require('babel-polyfill');
 import * as express from 'express';
 import {applyRoutes} from './modules/index';
 import * as bodyParser from 'body-parser';
+import db from './database/index';
+import User from './modules/user/usersModel';
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/api/v1', applyRoutes());
+
+app.get('/users', (req, res) => {
+    User.query()
+        .select('*')
+        .then(result => {
+            res.json(result);
+        });
+});
+
+app.post('/users', (req, res) => {
+    User.create(req.body)
+        .then(result => {
+            return res.json(result);
+        })
+});
 
 
 app.listen(40000, () => {
--- src/modules/user/userController.ts
+++ src/modules/user/userController.ts
@@ -1,25 +1,43 @@
 import * as express from "express";
+import User from './usersModel';
+import {success, error} from '../../util';
 
 export default class UserController {
 
     static async index(req: express.Request, res: express.Response) {
-        res.json({success: true});
+        try {
+            res.json(success(await User.all()))
+        } catch (e) {
+            res.json(error(e));
+        }
     }
 
     static async read(req: express.Request, res: express.Response) {
-        res.json({success: true});
+        try {
+            res.json(success(await User.one(req.params.id)));
+        } catch (e) {
+            res.json(error(e));
+        }
     }
 
     static async add(req: express.Request, res: express.Response) {
-        res.json({success: true});
+        try {
+            res.json(success(await User.create(req.body)));
+        } catch (e) {
+            res.json(error(e));
+        }
     }
 
     static async update(req: express.Request, res: express.Response) {
-        res.json({success: true});
+        try {
+            res.json(success(await User.update(req.params.id, req.body)));
+        } catch (e) {
+            res.json(error(e));
+        }
     }
 
     static async remove(req: express.Request, res: express.Response) {
-        res.json({success: true});
+        // NYI
     }
     
 }
