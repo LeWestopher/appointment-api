0b70437cf3d1f7752857edd1496be88f05b9a965
Coty Abadie <Coty_Abadie@yahoo.com>
2017-01-23 16:32:16 -0600

    added some models.

---
+++ src/modules/appointments/appointmentsModel.ts
@@ -0,0 +1,24 @@
+import * as express from "express";
+
+export default class AppointmentModel {
+    static index() {
+        
+    }
+    
+    static read() {
+        
+    }
+
+    static add() {
+        
+    }
+
+    static update() {
+        
+    }
+
+    static remove() {
+        
+    }
+
+}
---
+++ src/modules/user/userModel.ts
@@ -0,0 +1,23 @@
+import * as express from "express";
+
+export default class UserModel {
+    static index() {
+        
+    }
+    
+    static read() {
+        
+    }
+
+    static add() {
+        
+    }
+
+    static update() {
+        
+    }
+
+    static remove() {
+        
+    }
+}
